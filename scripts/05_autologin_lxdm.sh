fd1c76a505a3e061380517dac41e1ce7760d236c
arllinux <jeanpierre.antinoux@free.fr>
2018-01-16 20:40:50 +0100

    Autologin lxdm

---
+++ scripts/05_autologin_lxdm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# 06_autologin_lxdm.sh
+
+# Ce script permet de configurer l'autologin de l'utilisteur
+# de votre choix :
+# Jean-Pierre Antinoux - Janvier 2018 
+
+CWD=$(pwd)
+
+# Vérification de la syntaxe de l'utilisateur principal
+if [ $USER != "root" ]
+    then
+      echo "Pour exécuter ce script il faut être l'utilisateur root !"
+    else
+		# tester si le programme gawk est intallé, sinon le faire.
+		# cards info gawk | grep Nom | grep gawk | cut -d : -f2
+
+    # Vérification du nom d'utilisateur
+    read -p 'Utilisateur (login) à personnaliser : ' nom
+    while [ -z $nom ]; do
+    echo "Veuillez saisir votre nom"
+    read nom
+    done
+    cat /etc/passwd | grep bash | gawk -F ":" '{print $1}' | grep -w $nom > /dev/null
+    if [ $? = "0" ]
+		then
+  		# Modifier le fichier lxdm.conf
+  		cp /etc/lxdm/lxdm.conf /etc/lxdm/lxdm.conf.ofd
+  		cat $CWD/../lxdm/lxdm.conf > /etc/lxdm/lxdm.conf
+  	else
+			echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+			echo "Le fichier n'a pas pu être modifié !!"
+			echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+		fi
+fi
+
+exit 0
+

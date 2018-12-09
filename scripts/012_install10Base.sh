e0d3f7fc25e534a705408285a5ac6f787fc05281
arllinux <jeanpierre.antinoux@free.fr>
2018-12-09 21:50:03 +0100

    ...

--- scripts/012_install10Base.sh
+++ scripts/012_install10Base.sh
@@ -13,19 +13,48 @@ CWD=$(pwd)
     then
       echo "Pour exécuter ce script il faut être l'utilisateur root !"
     else
- 
+
+	 # Choix de l'environnement de bureau
+
    # Installer les outils nécessaires
- 	 # Voir pkglist/paquets4 
      echo "==============================================================="
      echo "==              Installation de quelques outils              =="
-     echo "==                    Ajout de paquets.                      =="
+     echo "==                       Choix du bureau                     =="
+		 echo "== Bureau Mate  = 1                                          =="
+		 echo "== Bureau kde5  = 2                                          =="
+		 echo "== Bureau Xfce4 = 3                                          =="
+		 echo "== Bureau lxde  = 4                                          =="
      echo "==============================================================="
-     PAQUETS4=$(egrep -v '(^\#)|(^\s+$)' $CWD/../pkglists/paquets4)
-     cards install $PAQUETS4
-     echo "==============================================================="
-     echo "==   Les utilitaires de base ainsi que mate, mate-desktop   ==="
-     echo "==               et lxdm ont été installés                  ==="
-     echo "==============================================================="
+     read -p 'Votre choix de bureau : ' num
+		 	if [ $num = "1" ]
+				then
+        PAQUETS4=$(egrep -v '(^\#)|(^\s+$)' $CWD/../pkglists/paquets4)
+        cards install $PAQUETS4
+      else
+		 	if [ $num = "2" ]
+				then
+        PAQUETS2=$(egrep -v '(^\#)|(^\s+$)' $CWD/../pkglists/kde5)
+        cards install $PAQUETS2
+      else
+		 	if [ $num = "3" ]
+				then
+        PAQUETS3=$(egrep -v '(^\#)|(^\s+$)' $CWD/../pkglists/xfce4)
+        cards install $PAQUETS3
+      else
+		 	if [ $num = "4" ]
+				then
+        PAQUETS4=$(egrep -v '(^\#)|(^\s+$)' $CWD/../pkglists/lxde)
+        cards install $PAQUETS4
+      else
+				exit
+		  fi
+		 fi
+		fi
+	 fi
+   echo "==============================================================="
+   echo "==   Les utilitaires de base ainsi que mate, mate-desktop   ==="
+   echo "==               et lxdm ont été installés                  ==="
+   echo "==============================================================="
 
  fi
 

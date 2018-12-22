020bb236abe2ac7dd501f638a6f4efdf8a982b0e
root <jeanpierre.antinoux@free.fr>
2018-12-22 17:25:41 +0100

    Modif script 07_lxdm.sh

--- scripts/07_lxdm.sh
+++ scripts/07_lxdm.sh
@@ -13,25 +13,12 @@ SLO10rc='http://sloteur.free.fr/arllinux/nutyx10rc_0118'
 WAYLXDM='/usr/share/lxdm/themes/Industrial/'
 #####################################################
 
-# Vérification de la syntaxe de l'utilisateur principal
-    # Vérification du nom d'utilisateur
-     read -p 'Utilisateur (login) à personnaliser : ' nom
-     while [ -z $nom ]; do
-     echo "Veuillez saisir votre nom"
-     read nom
-     done
-     cat /etc/passwd | grep bash | gawk -F ":" '{print $1}' | grep -w $nom > /dev/null
-       if [ $? = "0" ]
-         then
-         # lxdm
-         cd $WAYLXDM
-         wget $SLO10rc/$LXDM
-				 tar xvf $LXDM
-         chown root:root nutyx.jpg
-				 if [ -f "nutyx.jpg" ]; then
-					 rm $LXDM
-         fi
-      else
-         echo "Ce nom d'utilisateur n'existe pas. Réessayez !"
-      fi
+ # lxdm
+   cd $WAYLXDM
+   wget $SLO10rc/$LXDM
+	 tar xvf $LXDM
+	 chown root:root nutyx.jpg
+	 if [ -f "nutyx.jpg" ]; then
+      rm $LXDM
+   fi
 exit 0
